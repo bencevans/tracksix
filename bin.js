d4f9c620993242fc67e71930ffa32f3d98ee97c5
Ben Evans <ben@bluechimp.io>
2019-02-13 17:01:07 +0000

    chore: cli logging

--- bin.js
+++ bin.js
@@ -6,6 +6,8 @@ const config = tracksix.readConfigSync(process.argv[2])
 
 const tracker = tracksix(config)
 
+tracker.on('info', console.log)
+tracker.on('warn', console.warn)
 tracker.on('error', err => {
   console.error(err)
   process.exit(1)
